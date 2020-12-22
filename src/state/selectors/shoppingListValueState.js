43bbabbd1a78adfe12b93df196659ce3f68b98ec
Simon Boeuf <sboeuf@xebia.fr>
2020-12-22 10:41:20 +0100

    use selector

--- src/state/selectors/shoppingListValueState.js
+++ src/state/selectors/shoppingListValueState.js
@@ -1,11 +1,11 @@
-import {atom} from 'recoil'
+import {selector} from 'recoil'
 import { shoppingListState } from '../atoms/shoppingListState';
 
-export const shoppingListValueState = atom({
+export const shoppingListValueState = selector({
   key: 'shoppingListValueState',
   get: ({get}) => {
     const shoppingList = get(shoppingListState);
     return shoppingList.length;
   },
   default: 0
-  });
+});
